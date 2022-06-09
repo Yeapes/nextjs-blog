0286f21be70e9d9f297175f4558f8ebb67a1b057
yeapes <yeapesh2831@gmail.com>
2022-06-09 15:00:44 +0600

    Initial Commit

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <div class="py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+      <h1 className="title"> 
+        <Link href="/posts/posts">
+          <a className="text-red-400 border-lime-300">Posts</a>
+        </Link> 
+      </h1>
+
+      <h1 className="title"> 
+        <Link href="/responsive/responsive">
+          <a className="text-red-400 border-lime-300">Responsive</a>
+        </Link> 
+      </h1>
+      
+    </div>
+  )
+}
---
+++ pages/posts/posts.js
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+
+export default function Post() {
+  return (
+    <div class="py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+        <h1>
+            I'm From Post 
+        </h1>
+    </div>
+  )
+}
