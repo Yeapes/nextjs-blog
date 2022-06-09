6d142ae5d0a6cedc435475661744ba9438604ff6
yeapes <yeapesh2831@gmail.com>
2022-06-09 15:07:49 +0600

    Home link added

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,11 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
+    <>
+    <Head>
+      <title>Home Page</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
     <div class="py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
       <h1 className="title"> 
         <Link href="/posts/posts">
@@ -17,5 +22,6 @@ export default function Home() {
       </h1>
       
     </div>
+    </>
   )
 }
