15569723ec9a5ab26dab474ef8642b724e10f752
yeapes <yeapesh2831@gmail.com>
2022-06-09 18:29:32 +0600

    Markdown data fetch & Lib added for data fetching

--- pages/index.js
+++ pages/index.js
@@ -2,21 +2,43 @@ import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 
-export default function Home() {
+import { getSortedPostsData } from '../lib/posts';
+
+
+export default function Home({ allPostsData }) {
+  console.log(allPostsData);
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Hello! I'm Yeapes From Bangladesh, I have beeny  working over the 2 years. I like to doing code with PHP & Laravel Now I'm Learning Next Js. It's Really Awesome]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
+        <p>[Hello! I'm Yeapes From Bangladesh, I have been  working over the 2 years. I like to doing code with PHP & Laravel Now I'm Learning Next Js. It's Really Awesome]</p> 
+      </section>
 
-        
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
 }
