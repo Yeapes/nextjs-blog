af5aaf0b0023432752fbfe0562038fb3bcece1c7
yeapes <yeapesh2831@gmail.com>
2022-06-11 13:24:49 +0600

    Polishing The post page

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,17 +1,26 @@
 import Layout from '../../components/layout';
 import { getAllPostIds, getPostData } from '../../lib/posts';
+import Date from '../../components/date';
+import utilStyles from '../../styles/utils.module.css';
 
-export default function Post({postData}) {
+// Changing Header Data
+import Head from 'next/head';
+
+export default function Post({ postData }) {
     return (
         <Layout>
-          {postData.title}
-          <br />
-          {postData.id}
-          <br />
-          {postData.date}
-          <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+            <Head>
+                <title>{postData.title}</title>
+            </Head>
+            <article>
+                <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+                <div className={utilStyles.lightText}>
+                    <Date dateString={postData.date} />
+                </div>
+                <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+            </article>
         </Layout>
-      );
+    );
 }
 
 export async function getStaticPaths() {
@@ -19,8 +28,8 @@ export async function getStaticPaths() {
 
     const paths = getAllPostIds();
     return {
-      paths,
-      fallback: false,
+        paths,
+        fallback: false,
     };
 
 }
@@ -31,9 +40,9 @@ export async function getStaticProps({ params }) {
 
     const postData = await getPostData(params.id);
     return {
-      props: {
-        postData,
-      },
+        props: {
+            postData,
+        },
     };
 
 
