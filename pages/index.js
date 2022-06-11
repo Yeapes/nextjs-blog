8871c7b4c86e6700d66c025ffd47e0c051667d78
yeapes <yeapesh2831@gmail.com>
2022-06-11 16:08:55 +0600

    Home Page polishing

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
+import Link from 'next/link';
+import Date from '../components/date';
 
 import { getSortedPostsData } from '../lib/posts';
 
@@ -13,7 +15,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Hello! I'm Yeapes From Bangladesh, I have been  working over the 2 years. I like to doing code with PHP & Laravel Now I'm Learning Next Js. It's Really Awesome]</p> 
+        <p>[Hello! I'm Yeapes From Bangladesh, I have been  working over the 2 years. I like to doing code with PHP & Laravel Now I'm Learning Next Js. It's Really Awesome]</p>
       </section>
 
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
@@ -21,11 +23,13 @@ export default function Home({ allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
