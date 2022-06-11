e856f8664c7f6eb40198ab1404d7a625dcb827e2
yeapes <yeapesh2831@gmail.com>
2022-06-11 16:22:13 +0600

    link update

--- components/layout.js
+++ components/layout.js
@@ -60,7 +60,7 @@ export default function Layout({ children, home }) {
             </h2>
           </>
         )}
-        <Link href="/posts/first-post">
+        <Link href="/posts/ssg-ssr2">
             <a className='text-pink-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150'>First Post</a>
           </Link>
       </header>
