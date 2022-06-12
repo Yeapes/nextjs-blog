0919eaaa4ef58cc9ba9ec2f8d1f1b37e5c855814
yeapes <yeapesh2831@gmail.com>
2022-06-12 10:42:38 +0600

    Gallery with Tailwind css

--- pages/gallery/gallery.js
+++ pages/gallery/gallery.js
@@ -1,9 +1,33 @@
 import Layout from "../../components/layout";
-
-export default function gallery(){
+export default function gallery({ images }) {
     return (
-        <Layout>
-            Gallery
+        <Layout> 
+            <section className="overflow-hidden text-gray-700 ">
+                <div className="container px-5 py-2 mx-auto lg:pt-12 lg:px-32">
+                    <div className="flex flex-wrap -m-1 md:-m-2">
+                        {images.slice(0, 30).map((image) => (
+                            <div className="flex flex-wrap w-1/3">
+                                <div className="w-full p-1 md:p-2">
+                                    <img alt="gallery" className="block object-cover object-center w-full h-full rounded-lg"
+                                        src={image.thumbnailUrl} />
+                                </div>
+                            </div>
+                        ))} 
+                    </div>
+                </div>
+            </section>
         </Layout>
     );
+}
+
+export async function getStaticProps() {
+
+    const res = await fetch("https://jsonplaceholder.typicode.com/photos");
+    const images = await res.json();
+
+    return {
+        props: {
+            images: images,
+        }
+    }
 }
