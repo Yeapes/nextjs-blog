8b2e062a3ace17578cd9cdd4c275201aa6a40098
yeapes <yeapesh2831@gmail.com>
2022-06-12 10:22:20 +0600

    Gallery Link add in Layout

--- components/layout.js
+++ components/layout.js
@@ -63,6 +63,9 @@ export default function Layout({ children, home }) {
         <Link href="/posts/ssg-ssr2">
             <a className='text-pink-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150'>First Post</a>
           </Link>
+        <Link href="/gallery/gallery">
+            <a className='text-pink-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150'>Gallery</a>
+          </Link>
       </header>
 
       <main>{children}</main>
---
+++ pages/gallery/gallery.js
@@ -0,0 +1,9 @@
+import Layout from "../../components/layout";
+
+export default function gallery(){
+    return (
+        <Layout>
+            Gallery
+        </Layout>
+    );
+}
