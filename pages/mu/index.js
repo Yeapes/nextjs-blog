2cd5c9be7b1ac416e6e4894bda65dc2ea7e9cbaa
yeapes <yeapesh2831@gmail.com>
2022-07-04 16:20:53 +0600

    material ui login design added

---
+++ pages/mu/index.js
@@ -0,0 +1,12 @@
+import { Button } from "@mui/material";
+import Layout from "../../components/layout";
+function Mu(){ 
+    return(
+        <Layout>
+            <Button>Hello world from MU</Button> 
+        </Layout>
+    );
+}
+
+
+export default Mu;
